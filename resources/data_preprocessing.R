1cf4eb2b7b91ba717e3b6a3a57539a7197a2b09b
joywang1999 <jinyuwang1999@gmail.com>
2021-12-12 05:35:20 -0500

    Wrote some of chapter 3, output kable still not scrollable

--- resources/data_preprocessing.R
+++ resources/data_preprocessing.R
@@ -14,26 +14,34 @@ years_to_use <- list(
   "2020" = "nyts2020.xlsx",
   "2019" = "nyts2019.xlsx",
   "2018" = "nyts2018.xlsx",
-  "2017" = "nyts2017.xlsx"
+  "2017" = "nyts2017.xlsx",
+  "2016" = "nyts2016.xlsx"
 )
 
 codebook <- read_excel("data/nyts-codebook.xlsx")
 
 path <- "data/NYTS/"
 
-years = c("2020", "2019", "2018")
+years = c("2020", "2019", "2018", "2017", "2016")
 
 columns <- colnames(codebook) 
 final <- data.frame(matrix(nrow=0, ncol=length(columns)))
 colnames(final) <- columns
 
+missing <- c(".", "E", "N", "S", "Z", ".N", ".S", ".Z", "*", "**")
+
 for (y in years) {
   df <- read_excel(paste0(path, years_to_use[[y]]))
   questions <- codebook %>% filter(Year == y) %>% select(-Year) %>% unlist(., use.names = FALSE)
   selected_df <- df %>% mutate(Year=y) %>% select(Year, all_of(questions))
+  
+  # recode some characters meaning missing responses to NA
+  for (x in missing) {
+    selected_df <- selected_df %>% mutate(across(everything(), na_if, x))
+  }
+  
   colnames(selected_df) <- columns
   final <- rbind(final, selected_df)
 }
 
 write_excel_csv(final, "data/nyts_grouped.csv")
-
